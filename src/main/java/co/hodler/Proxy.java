75775c4700796e3f1e18dd5bbd6c86681a5892dd
xorrr <derxor@gmail.com>
2015-03-17 00:09:04 +0100

    add three approaches to testing the proxy

--- src/main/java/co/hodler/Proxy.java
+++ src/main/java/co/hodler/Proxy.java
@@ -1,5 +1,32 @@
 package co.hodler;
 
+import org.eclipse.jetty.proxy.ConnectHandler;
+import org.eclipse.jetty.proxy.ProxyServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 public class Proxy {
+
+  public static void main(String[] args) throws Exception {
+    Proxy p = new Proxy(8888);
+  }
+
+  public Proxy(int port) throws Exception {
+    Server server = new Server();
+    ServerConnector connector = new ServerConnector(server);
+    connector.setPort(port);
+    server.addConnector(connector);
+
+    ConnectHandler proxy = new ConnectHandler();
+    server.setHandler(proxy);
+
+    ServletContextHandler context = new ServletContextHandler(proxy, "/", ServletContextHandler.SESSIONS);
+    ServletHolder proxyServlet = new ServletHolder(ProxyServlet.class);
+    context.addServlet(proxyServlet, "/*");
+
+    server.start();
+  }
 
 }
--- src/test/java/co/hodler/ProxyTest.java
+++ src/test/java/co/hodler/ProxyTest.java
@@ -1,6 +1,59 @@
 package co.hodler;
 
+import static org.junit.Assert.assertEquals;
+
+import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
 
 public class ProxyTest {
 
+  @Test
+  public void withApacheHttpClient() throws Exception {
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+    try {
+      HttpHost target = new HttpHost("www.google.de");
+      HttpHost proxy = new HttpHost("localhost", 8888);
+
+      RequestConfig config = RequestConfig.custom().setProxy(proxy).build();
+      HttpGet request = new HttpGet();
+      request.setConfig(config);
+
+      CloseableHttpResponse response = httpclient.execute(target, request);
+      try {
+        assertEquals(200, response.getStatusLine().getStatusCode());
+        EntityUtils.consume(response.getEntity());
+      } finally {
+        response.close();
+      }
+    } finally {
+      httpclient.close();
+    }
+  }
+
+  @Test
+  public void withUniRest() throws Exception {
+    Unirest.setProxy(new HttpHost("localhost", 8888));
+
+    HttpResponse<String> jsonResponse = Unirest.get("http://www.zeit.de")
+        .asString();
+
+    assertEquals(200, jsonResponse.getStatus());
+  }
+
+  @Test
+  public void withRestAssured() {
+    RestAssured.proxy(8888);
+
+    assertEquals(200, RestAssured.get("http://www.ebay.de").statusCode());
+  }
 }
